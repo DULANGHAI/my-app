e3d8c163370ac13e6f90c14a12ca27c3bf1fec72
wanglin <wanglin@zhongan.io>
2018-09-11 17:57:54 +0800

    暂存

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,91 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <banner />
+    <grid />
+    <block />
+    <list />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Banner from '@/components/sample/Banner'
+import Grid from '@/components/sample/Grid'
+import Block from '@/components/sample/Block'
+import List from '@/components/sample/List'
+
+export default {
+  name: 'App',
+  components: {
+    Banner,
+    Grid,
+    Block,
+    List
   }
+}
+</script>
+
+<style lang="scss">
+// 全局样式
+html {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  background-color: #f4f4f4;
+}
+* {
+  box-sizing: inherit;
+
+  &:before,
+  &:after {
+    box-sizing: inherit;
+  }
+}
+img {
+  width: 100%;
+  height: auto;
+  vertical-align: top;
+  content: normal !important;
+}
+[aspectratio] {
+  position: relative;
+}
+[aspectratio]::before {
+  content: '';
+  display: block;
+  width: 1px;
+  margin-left: -1px;
+  height: 0;
+}
+
+[aspectratio-content] {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+}
+[flexContainer] {
+  display: flex;
+  width: 750px;
+}
+.block {
+  margin-top: 15px;
+}
+ul,li {
+  list-style: none outside none;
+  margin: 0;
+  padding: 0;
+}
+figure {
+  display: block;
+  margin: 0;
 }
 </style>
